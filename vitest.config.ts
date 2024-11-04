37a66a699742ba6f991757b8872869c249f7ebb7
Gustavo Targino <gustavo.targinocc@hotmail.com>
2024-11-03 22:19:15 -0300

    feat: Implementando testes na paginação

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
 // vitest.config.ts
+import { resolve } from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
   test: {
     globals: true,
     setupFiles: ["./test/setup.ts"],
